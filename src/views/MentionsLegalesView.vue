5a80fa96f724e1b95e136257375f5ef63688de2b
aeccher <95070759+aeccher@users.noreply.github.com>
2022-05-30 16:44:12 +0200

    page_mentionslegales

--- src/views/MentionsLegalesView.vue
+++ src/views/MentionsLegalesView.vue
@@ -1,6 +1,27 @@
 <template>
   <main>
-    <h1>Contenu de Mentions légales</h1>
+    <div class="m-9">
+      <p class="font-roboto text-xs italic">DIRECTION DE LA PUBLICATION</p>
+      <p class="font-mate text-xs">Ziclon festival</p>
+      <p class="font-mate text-xs">ziclon@festival.com</p>
+      <p class="font-mate text-xs">03 48 45 52 18</p>
+      <p class="font-roboto text-xs italic">Domaine</p>
+      <p class="font-mate text-xs">ziclonfestival.fr</p>
+      <p class="font-roboto text-xs italic">Droits d'utilisation et de reproduction</p>
+      <p class="font-mate text-xs">
+        Le droit de reproduction et/ou droit de représentation des données du site web ziclonfestival.fr est exclusivement destiné à l'usage
+        privé et/ou aux seules fins de consultation personnelle et privée des utilisateurs du réseau Internet.
+      </p>
+      <p class="font-mate text-xs">
+        Toute autre utilisation est strictement interdite et susceptible de poursuites conformément aux dispositions du Code de la Propriété
+        Intellectuelle français, des règlements nationaux et des conventions internationales en vigueur.
+      </p>
+      <p class="font-mate text-xs">Logo : Le logo du Ziclon est la propriété du Ziclon Festival.</p>
+      <p class="font-mate text-xs">
+        Toute reproduction totale ou partielle de cette marque sans autorisation préalable et écrite est interdite, de même que toute
+        modification des proportions, couleurs, éléments et constituants.
+      </p>
+    </div>
   </main>
 </template>
 
