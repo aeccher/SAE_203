569d332a21ec1e1b1225c4e596a2f3a9dcfdc6f5
aeccher <95070759+aeccher@users.noreply.github.com>
2022-05-30 15:47:59 +0200

    page_artistes+card_artistes

---
+++ src/components/CardArtistes.vue
@@ -0,0 +1,28 @@
+<template >
+  <div class="w-72 rounded-lg bg-Noir">
+    <img :src="img_card" />
+
+    <p class="pl-6 font-mate text-xl text-Vert">
+      {{ title }}
+    </p>
+
+    <icon-line class="pl-6" />
+
+    <p class="pl-6 font-mate text-xs italic text-Vert">
+      {{ lieu }}
+    </p>
+  </div>
+</template>
+
+<script >
+import IconLine from "./icons/IconLine.vue";
+
+export default {
+  components: { IconLine },
+  props: {
+    title: { type: String },
+    lieu: { type: String },
+    img_card: String,
+  },
+};
+</script>
--- src/views/ArtistesView.vue
+++ src/views/ArtistesView.vue
@@ -1,9 +1,53 @@
 <template>
-  <main>
-    <h1>Contenu de Artistes</h1>
+  <main class="grid gap-12 gap-x-4 bg-Bleu pl-36 pt-14 pb-16 ipad_mini:grid-cols-2 bureau:grid-cols-3">
+    <card-artistes
+      title="Cendres"
+      lieu="FRANCE / Châlons-sur-Saône – Métal"
+      img_card="src/assets/card_image1.jpg"
+      alt="visuel du groupe Cendres"
+    />
+
+    <card-artistes
+      title="2PanHeads"
+      lieu="FRANCE / Metz Rock, Post Punk"
+      img_card="src/assets/card_image2.jpg"
+      alt="visuel du groupe 2PanHeads"
+    />
+
+    <card-artistes
+      title="#TRAD'"
+      lieu="FRANCE / Gannat Traditionnel du Massif central"
+      img_card="src/assets/card_image3.jpg"
+      alt="visuel du groupe #TRAD'"
+    />
+
+    <card-artistes
+      title="AMA meets Bandikoot"
+      lieu="FRANCE / Burkina Faso  Electro dub"
+      img_card="src/assets/card_image4.jpg"
+      alt="visuel du groupe AMA meets Bandikoot"
+    />
+
+    <card-artistes
+      title="Junge sinfoniker Frankfurt"
+      lieu="ALLEMAGNE / Orchestre"
+      img_card="src/assets/card_image5.jpg"
+      alt="visuel du groupe Junge sinfoniker Frankfurt"
+    />
+
+    <card-artistes
+      title="Al pari quartet"
+      lieu="POLOGNE / Quatuor à cordes"
+      img_card="src/assets/card_image6.jpg"
+      alt="visuel du groupe Al pari quartet"
+    />
   </main>
 </template>
 
 <script>
-export default {};
+import CardArtistes from "../components/CardArtistes.vue";
+
+export default {
+  components: { CardArtistes },
+};
 </script>
