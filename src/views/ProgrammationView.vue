3a43295e5a5d98819291c268fecf3abecd62fdde
aeccher <95070759+aeccher@users.noreply.github.com>
2022-05-30 13:56:57 +0200

    page_programmation_version_mobile

--- src/views/ProgrammationView.vue
+++ src/views/ProgrammationView.vue
@@ -8,6 +8,33 @@
         img_card="src/assets/card_image2.jpg"
         alt="visuel du groupe 2PanHeads"
       />
+      <div class="pt-12">
+        <card-programmation title="#TRAD'" genre="Musiques du monde" img_card="src/assets/card_image3.jpg" alt="visuel du groupe #TRAD'" />
+      </div>
+    </div>
+    <div class="pt-12">
+      <card-programmation
+        title="AMA meets Bandikoot"
+        genre="Musiques du monde"
+        img_card="src/assets/card_image4.jpg"
+        alt="visuel du groupe AMA meets Bandikoot"
+      />
+    </div>
+    <div class="pt-12">
+      <card-programmation
+        title="Junge sinfoniker Frankfurt"
+        genre="Musiques classiques"
+        img_card="src/assets/card_image5.jpg"
+        alt="visuel du groupe Junge sinfoniker Frankfurt"
+      />
+    </div>
+    <div class="pt-12">
+      <card-programmation
+        title="Al pari quartet"
+        genre="Musiques classiques"
+        img_card="src/assets/card_image6.jpg"
+        alt="visuel du groupe Al pari quartet"
+      />
     </div>
   </main>
 </template>
