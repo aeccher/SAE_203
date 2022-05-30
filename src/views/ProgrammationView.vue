7d2c323821452f22605b97871e686cfa8cd85ea7
aeccher <95070759+aeccher@users.noreply.github.com>
2022-05-30 14:31:15 +0200

    page_programmation_responsive

--- src/views/ProgrammationView.vue
+++ src/views/ProgrammationView.vue
@@ -1,41 +1,36 @@
 <template>
-  <main class="grid place-content-center bg-Vert pt-14 pb-16">
+  <main class="grid gap-12 gap-x-4 bg-Vert pl-36 pt-14 pb-16 ipad_mini:grid-cols-2 bureau:grid-cols-3">
     <card-programmation title="Cendres" genre=" Musiques actuelles" img_card="src/assets/card_image1.jpg" alt="visuel du groupe Cendres" />
-    <div class="pt-12">
-      <card-programmation
-        title="2PanHeads"
-        genre=" Musiques actuelles"
-        img_card="src/assets/card_image2.jpg"
-        alt="visuel du groupe 2PanHeads"
-      />
-      <div class="pt-12">
-        <card-programmation title="#TRAD'" genre="Musiques du monde" img_card="src/assets/card_image3.jpg" alt="visuel du groupe #TRAD'" />
-      </div>
-    </div>
-    <div class="pt-12">
-      <card-programmation
-        title="AMA meets Bandikoot"
-        genre="Musiques du monde"
-        img_card="src/assets/card_image4.jpg"
-        alt="visuel du groupe AMA meets Bandikoot"
-      />
-    </div>
-    <div class="pt-12">
-      <card-programmation
-        title="Junge sinfoniker Frankfurt"
-        genre="Musiques classiques"
-        img_card="src/assets/card_image5.jpg"
-        alt="visuel du groupe Junge sinfoniker Frankfurt"
-      />
-    </div>
-    <div class="pt-12">
-      <card-programmation
-        title="Al pari quartet"
-        genre="Musiques classiques"
-        img_card="src/assets/card_image6.jpg"
-        alt="visuel du groupe Al pari quartet"
-      />
-    </div>
+
+    <card-programmation
+      title="2PanHeads"
+      genre=" Musiques actuelles"
+      img_card="src/assets/card_image2.jpg"
+      alt="visuel du groupe 2PanHeads"
+    />
+
+    <card-programmation title="#TRAD'" genre="Musiques du monde" img_card="src/assets/card_image3.jpg" alt="visuel du groupe #TRAD'" />
+
+    <card-programmation
+      title="AMA meets Bandikoot"
+      genre="Musiques du monde"
+      img_card="src/assets/card_image4.jpg"
+      alt="visuel du groupe AMA meets Bandikoot"
+    />
+
+    <card-programmation
+      title="Junge sinfoniker Frankfurt"
+      genre="Musiques classiques"
+      img_card="src/assets/card_image5.jpg"
+      alt="visuel du groupe Junge sinfoniker Frankfurt"
+    />
+
+    <card-programmation
+      title="Al pari quartet"
+      genre="Musiques classiques"
+      img_card="src/assets/card_image6.jpg"
+      alt="visuel du groupe Al pari quartet"
+    />
   </main>
 </template>
 
