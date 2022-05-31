aff36512fd247be0d8e45dd6d5b8fec508ef8a90
aeccher <95070759+aeccher@users.noreply.github.com>
2022-05-31 08:43:39 +0200

    modification_page_mentionslegales_mise_en_page

--- src/views/MentionsLegalesView.vue
+++ src/views/MentionsLegalesView.vue
@@ -1,23 +1,25 @@
 <template>
   <main>
-    <div class="m-9">
-      <p class="font-roboto text-xs italic">DIRECTION DE LA PUBLICATION</p>
-      <p class="font-mate text-xs">Ziclon festival</p>
-      <p class="font-mate text-xs">ziclon@festival.com</p>
-      <p class="font-mate text-xs">03 48 45 52 18</p>
-      <p class="font-roboto text-xs italic">Domaine</p>
-      <p class="font-mate text-xs">ziclonfestival.fr</p>
-      <p class="font-roboto text-xs italic">Droits d'utilisation et de reproduction</p>
-      <p class="font-mate text-xs">
+    <div class="m-9 flex flex-col space-y-10">
+      <p class="font-roboto text-xs italic ipad_mini:text-2xl ipad_mini:font-bold ipad_mini:not-italic">DIRECTION DE LA PUBLICATION</p>
+      <p class="font-mate text-xs ipad_mini:text-lg">Ziclon festival</p>
+      <p class="font-mate text-xs ipad_mini:text-lg">ziclon@festival.com</p>
+      <p class="font-mate text-xs ipad_mini:text-lg">03 48 45 52 18</p>
+      <p class="font-roboto text-xs italic ipad_mini:text-2xl ipad_mini:font-bold ipad_mini:not-italic">Domaine</p>
+      <p class="font-mate text-xs ipad_mini:text-lg">ziclonfestival.fr</p>
+      <p class="font-roboto text-xs italic ipad_mini:text-2xl ipad_mini:font-bold ipad_mini:not-italic">
+        Droits d'utilisation et de reproduction
+      </p>
+      <p class="font-mate text-xs ipad_mini:text-lg ipad_mini:leading-8">
         Le droit de reproduction et/ou droit de représentation des données du site web ziclonfestival.fr est exclusivement destiné à l'usage
         privé et/ou aux seules fins de consultation personnelle et privée des utilisateurs du réseau Internet.
       </p>
-      <p class="font-mate text-xs">
+      <p class="font-mate text-xs ipad_mini:text-lg ipad_mini:leading-8">
         Toute autre utilisation est strictement interdite et susceptible de poursuites conformément aux dispositions du Code de la Propriété
         Intellectuelle français, des règlements nationaux et des conventions internationales en vigueur.
       </p>
-      <p class="font-mate text-xs">Logo : Le logo du Ziclon est la propriété du Ziclon Festival.</p>
-      <p class="font-mate text-xs">
+      <p class="font-mate text-xs ipad_mini:text-lg ipad_mini:leading-8">Logo : Le logo du Ziclon est la propriété du Ziclon Festival.</p>
+      <p class="font-mate text-xs ipad_mini:text-lg ipad_mini:leading-8">
         Toute reproduction totale ou partielle de cette marque sans autorisation préalable et écrite est interdite, de même que toute
         modification des proportions, couleurs, éléments et constituants.
       </p>
