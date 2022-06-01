ef3d82e99b72c37e31e0e7bbb2b2a2df0f7f5947
aeccher <95070759+aeccher@users.noreply.github.com>
2022-06-01 09:25:51 +0200

    Mise en place du back-end via Firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,39 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDMyrw6qICKdqZmM6hRKkpw-UNTLjJn4i4",
+
+    authDomain: "sae-203-3c951.firebaseapp.com",
+
+    projectId: "sae-203-3c951",
+
+    storageBucket: "sae-203-3c951.appspot.com",
+
+    messagingSenderId: "350606108976",
+
+    appId: "1:350606108976:web:e45539aa64197e771380f5"
+
+};
+
+
+// Initialize Firebase
+
+const appFirebase = initializeApp(firebaseConfig);
+
+
 const app = createApp(App)
 
 app.use(router)
