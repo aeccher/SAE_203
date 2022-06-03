f3a7e5530262169e33de473f7e6dc7892cb45a19
aeccher <95070759+aeccher@users.noreply.github.com>
2022-06-03 08:05:08 +0200

    suppression_card_programmation_tailwind

--- src/views/ProgrammationView.vue
+++ src/views/ProgrammationView.vue
@@ -11,45 +11,6 @@
         />
       </div>
     </Router-link>
-
-    <!-- <Router-link to="/concert">
-      <card-programmation
-        title="Cendres"
-        genre=" Musiques actuelles"
-        img_card="src/assets/card_image1.jpg"
-        alt="visuel du groupe Cendres"
-      />
-    </Router-link>
-
-    <card-programmation
-      title="2PanHeads"
-      genre=" Musiques actuelles"
-      img_card="src/assets/card_image2.jpg"
-      alt="visuel du groupe 2PanHeads"
-    />
-
-    <card-programmation title="#TRAD'" genre="Musiques du monde" img_card="src/assets/card_image3.jpg" alt="visuel du groupe #TRAD'" />
-
-    <card-programmation
-      title="AMA meets Bandikoot"
-      genre="Musiques du monde"
-      img_card="src/assets/card_image4.jpg"
-      alt="visuel du groupe AMA meets Bandikoot"
-    />
-
-    <card-programmation
-      title="Junge sinfoniker Frankfurt"
-      genre="Musiques classiques"
-      img_card="src/assets/card_image5.jpg"
-      alt="visuel du groupe Junge sinfoniker Frankfurt"
-    />
-
-    <card-programmation
-      title="Al pari quartet"
-      genre="Musiques classiques"
-      img_card="src/assets/card_image6.jpg"
-      alt="visuel du groupe Al pari quartet"
-    /> -->
   </main>
 </template>
 
